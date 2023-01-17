8ef0312cd9fcadddce52a80217261592f1fb632b
Agustí Pou Company <agustipcom@gmail.com>
2023-01-17 10:38:20 +0100

    Use a button

--- part1/componentState/src/index.js
+++ part1/componentState/src/index.js
@@ -6,15 +6,12 @@ const rootElement = document.getElementById("root");
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setInterval(() => {
-    setCounter(counter + 1);
-  }, 2000);
-
   return (
     <div>
       <h1>React Magic</h1>
       <p>The value of the counter is:</p>
-      <h2>{counter}</h2>;
+      <h2>{counter}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
     </div>
   );
 };
