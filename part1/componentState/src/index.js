288f318d919a27995223ec7117c47c5f7ed538e6
Agustí Pou Company <agustipcom@gmail.com>
2023-01-17 12:41:30 +0100

    Add reset button

--- part1/componentState/src/index.js
+++ part1/componentState/src/index.js
@@ -6,12 +6,29 @@ const rootElement = document.getElementById("root");
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+  const handleClickReset = () => {
+    setCounter(0);
+  };
   return (
     <div>
       <h1>React Magic</h1>
       <p>The value of the counter is:</p>
       <h2>{counter}</h2>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button
+        onClick={handleClick}
+        /* onClick={() => {
+          // setCounter(counter + 1);
+          setCounter((prevCounter) => {
+            return prevCounter + 1;
+          });
+        }} */
+      >
+        Increment
+      </button>
+      <button onClick={handleClickReset}>Reset</button>
     </div>
   );
 };
