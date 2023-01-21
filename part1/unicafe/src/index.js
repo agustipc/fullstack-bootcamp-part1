2083d1053062d0484ec2db6c50a30cd5711152f4
Agustí Pou Company <agustipcom@gmail.com>
2023-01-21 21:47:20 +0100

    Exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -45,7 +45,11 @@ const App = () => {
         variableSetter={setNeutral}
       />
       <Button title={"Bad"} variable={bad} variableSetter={setBad} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   )
 }
