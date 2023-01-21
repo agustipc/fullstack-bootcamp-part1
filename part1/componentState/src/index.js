b0fb01c03a5c1b4d2c8c1f2315348be3f56f4264
Agustí Pou Company <agustipcom@gmail.com>
2023-01-21 19:21:33 +0100

    Use only one state

--- part1/componentState/src/index.js
+++ part1/componentState/src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from "react-dom"
 import "./styles.css"
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [counters, setCounters] = useState({
+    left: 0,
+    right: 0,
+  })
+
+  const handleClickLeft = () => {
+    setCounters({ left: counters.left + 1, right: counters.right })
+  }
+  const handleClickRight = () => {
+    setCounters({ left: counters.left, right: counters.right + 1 })
+  }
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {counters.left}
+      <button onClick={handleClickLeft}>left</button>
+      <button onClick={handleClickRight}>right</button>
+      {counters.right}
     </div>
   )
 }
