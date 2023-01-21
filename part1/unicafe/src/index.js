312b35729d3a3fa06fa9ad3561611a589b2a6a66
Agustí Pou Company <agustipcom@gmail.com>
2023-01-21 21:24:27 +0100

    Exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import "./styles.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Button = ({ title, variable, variableSetter }) => {
+  return (
+    <button
+      onClick={() => {
+        variableSetter(variable + 1)
+      }}
+    >
+      {title}
+    </button>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <h1>GIVE FEEDBACK</h1>
+      <Button title={"Good"} variable={good} variableSetter={setGood} />
+      <Button
+        title={"Neutral"}
+        variable={neutral}
+        variableSetter={setNeutral}
+      />
+      <Button title={"Bad"} variable={bad} variableSetter={setBad} />
+      <h4>STATISTICS</h4>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
