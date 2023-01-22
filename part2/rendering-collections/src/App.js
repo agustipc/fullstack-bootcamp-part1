bc1ce8310cc59af04758ae6703d9358afd13b781
Agustí Pou Company <agustipcom@gmail.com>
2023-01-22 07:48:57 +0100

    Class 4 bootcamp

---
+++ part2/rendering-collections/src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css"
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+]
+
+export default function App() {
+  if (typeof notes === "undefined" || notes.length === 0) {
+    return "No hay notas"
+  }
+  return (
+    <div>
+      {notes.map((note) => {
+        return (
+          <div style={{ paddingBottom: "20px" }}>
+            <ul>
+              <small>{note.date}</small>
+            </ul>
+            <ul>{note.content}</ul>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
