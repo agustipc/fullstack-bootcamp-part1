3836531057b873bdec0ea0ddb8aacf311bf5967b
Agustí Pou Company <agustipcom@gmail.com>
2023-01-27 14:58:19 +0100

    Create all requests

--- part3/part3-fullstack-node/index.js
+++ part3/part3-fullstack-node/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -48,6 +50,24 @@ app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter((note) => note.id !== id)
   response.status(204).end()
+})
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body
+
+  const ids = notes.map((note) => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== "undefined" ? note.important : false,
+    date: new Date().toISOString(),
+  }
+
+  notes = [...notes, newNote]
+
+  response.json(newNote)
 })
 
 const PORT = 3001
