44cd4de2d429fe87c3d4b0706b3a37015630abf0
Agustí Pou Company <agustipcom@gmail.com>
2023-03-31 14:42:21 +0200

    Refactor tests

--- part3/notes-app/cypress/e2e/note_app.cy.js
+++ part3/notes-app/cypress/e2e/note_app.cy.js
@@ -15,6 +15,14 @@ describe('Note App', () => {
     cy.contains('Notes')
   })
 
+  it('should fail if credentials are wrong', () => {
+    cy.contains('Show Login').click()
+    cy.get('[name="Username"]').type('username')
+    cy.get('[name="Password"]').type('aaslkf')
+    cy.get('#form-login-button').click()
+    cy.get('.error').contains('Wrong credentials')
+  })
+
   it('user can login', () => {
     cy.contains('Show Login').click()
     cy.get('[name="Username"]').type(user.username)
@@ -27,15 +35,19 @@ describe('Note App', () => {
 
   describe('after login', () => {
     beforeEach(() => {
-      cy.contains('Show Login').click()
-      cy.get('[name="Username"]').type(user.username)
-      cy.get('[name="Password"]').type(user.password)
-      cy.get('#form-login-button').click()
-      cy.contains('Create a new note')
-      cy.get('#togglable-button').click()
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: user.username,
+        password: user.password
+      }).then((response) => {
+        localStorage.setItem('loggedNoteAppUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
     })
     it('can create a new note after login', () => {
       const noteContent = 'New note for testing'
+
+      cy.contains('Create a new note')
+      cy.get('#togglable-button').click()
       cy.get('[data-cy="note-form"] input[name="Add note"] ').type(noteContent)
       cy.get('[type="submit"]').click()
       cy.get('[data-cy="note-list-item"').last().contains(noteContent)
